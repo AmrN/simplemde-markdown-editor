01cfec69aa52ff29d1c8d184b98a3a159726f31d
Amr Noman <amr.noman1990@gmail.com>
2018-01-05 00:28:09 +0300

    working scrolling

--- example/index.js
+++ example/index.js
@@ -2,27 +2,67 @@ var simplemde = new SimpleMDE({ element: document.getElementById("root") });
 
 var scrollContainer = document.querySelector(".mde-wrapper");
 var codeMirrorEl = document.querySelector(".CodeMirror");
-var focusArea = document.querySelector(".focus-area");
+// var focusArea = document.querySelector(".focus-area");
 
 var cm = simplemde.codemirror;
 
 
 cm.on("scrollCursorIntoView", (inst, ev) => {
-  // console.log("scrollCursorIntoView");
-  const currentLine = inst.getCursor().line;
-  const linesCount = inst.lineCount();
+  const currentPos = inst.getCursor();
+  const hitSide = cm.findPosV(currentPos,3,'line',).hitSide;
 
-  if (linesCount - currentLine < 3) {
-    scrollContainer.scrollTop = scrollContainer.scrollHeight;
+  if (hitSide) {  
+    console.log('hit');
+    // scrollContainer.scrollTop = scrollContainer.scrollHeight;
   }
-  // console.log('totalLines:', lineCount)
-  // console.log(cursor);
+
+  // console.log('caret:', getCaretCoordinates());
+  // console.log('container:', scrollContainer.getBoundingClientRect());
+  scrollToCaret();
 });
 
-focusArea.addEventListener('click', (ev) => {
-  if (ev.target === focusArea) {
-    // set cursor to end of document
-    cm.setCursor(1000000);
-    cm.focus();
+// 
+
+function reachedEnd() {
+  const currentPos = cm.getCursor();
+  const hitSide = cm.findPosV(currentPos,3,'line',).hitSide;
+
+  if (hitSide) {  
+    console.log('hit');
+    return true;
   }
-})
+  return false;
+}
+
+function scrollToCaret() {
+  const cmCaret = cm.cursorCoords(true, 'window');
+  const caretPosY = cmCaret.bottom;
+  // const caretHeight = cmCaret.bottom - cmCaret.top;
+  const scrollContainerPosY = scrollContainer.getBoundingClientRect().y;
+  const scrollContainerHeight = scrollContainer.clientHeight;
+  let posDiff = 0;
+
+  if (cmCaret.top < scrollContainerPosY) {
+    console.log('minus');
+     posDiff = cmCaret.top - (scrollContainerPosY);
+
+  } else if (cmCaret.bottom > scrollContainerPosY + scrollContainerHeight) {
+     posDiff = cmCaret.bottom - (scrollContainerPosY + scrollContainerHeight);
+     console.log('positive:', posDiff);
+    } else {
+      console.log('inside');
+    }
+    scrollContainer.scrollTop += posDiff;
+
+    if (reachedEnd()) {
+      scrollContainer.scrollTop = scrollContainer.scrollHeight;
+    }
+}
+
+// focusArea.addEventListener('click', (ev) => {
+//   if (ev.target === focusArea) {
+//     // set cursor to end of document
+//     cm.setCursor(1000000);
+//     cm.focus();
+//   }
+// })
